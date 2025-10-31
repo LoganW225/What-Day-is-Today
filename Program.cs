7c8fc79c05c69d5ae8741d07c977e1c941c8bd84
LoganW225 <loganwise225@gmail.com>
2025-10-30 19:46:55 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace What_Day_is_Today
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
